91c094c4162ec1fc68952d212eef58ebe6b2cc09
Tim Suellentrop <tim@timsuellentrop.com>
2017-10-25 13:43:25 -0400

    adding topbar as above content widget

--- wp/homestephomecare/custom/bottom-CTA.php
+++ wp/homestephomecare/custom/bottom-CTA.php
@@ -12,7 +12,19 @@ function reach_widgets_init() {
          'after_title'   => '</h6>',
       )
   );
+  register_sidebar(
+      array(
+       'name' => __( 'Above Content', 'be-themes' ),
+       'id'   => 'reach-above-content',
+       'description'   => __( 'Widget area (above content)', 'be-themes' ),
+       'before_widget' => '<div class="%2$s widget">',
+       'after_widget'  => '</div>',
+       'before_title'  => '<h6>',
+       'after_title'   => '</h6>',
+    )
+  );
 }
+
 add_action( 'widgets_init', 'reach_widgets_init' );
 /* next put widget area in at the begining of the footer.php
 /* mods
